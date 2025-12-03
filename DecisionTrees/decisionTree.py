8673e259c786de3f7eaab8a219d89eb41b9c9bb1
Saman Ashoori <saman@samanashoori.com>
2025-12-03 22:17:02 +0000

    Refactor unique_vals function and enhance class_counts implementation with additional print statements for clarity

--- DecisionTrees/decisionTree.py
+++ DecisionTrees/decisionTree.py
@@ -14,7 +14,24 @@ training_data = [
 #Our goal will be to predict a fruit based on sie and colour and whether its sweet or sour.
 
 #print unique_values
-def unique_vals(rows,cols):
+def unique_vals(rows,col):
     return set([row[col] for row in rows])
 
+#Showcases unique values in the colour column 1 would be size , 2 would be sweet/sour and 3 would be label 
 print(unique_vals(training_data,0))
+
+def class_counts(rows):
+    #counts the number of each type of example in the dataset
+    counts = {}
+    for row in rows:
+        #the label is always the last column for our dataset format
+        label = row[-1]
+        if label not in counts:
+            counts[label] = 0
+        counts[label] +=1
+        
+    return counts
+
+print(class_counts(training_data))
+
+#Implementing ID3
